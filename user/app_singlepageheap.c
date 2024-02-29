a9475c36781ea24cd73f12bcc6c2baee566e36f1
Yiipu <jaap20035@outlook.com>
2024-02-29 02:20:09 +0000

    Update test case, pass test

--- user/app_singlepageheap.c
+++ user/app_singlepageheap.c
@@ -17,19 +17,21 @@ char* strcpy(char* dest, const char* src) {
 int main(void) {
   
   char str[20] = "hello, world!!!";
-  char *m = (char *)better_malloc(100);
-  char *p = (char *)better_malloc(50);
+  char *m = (char *)better_malloc(100); // 0~136 (100 roundup 8 to 104, add size of memblock 32, total 136)
+  char *p = (char *)better_malloc(50); // 136~224 (50 roundup 8 to 56, add size of memblock 32, total 88, 136 + 88=224)
   if((uint64)p - (uint64)m > 512 ){
     printu("you need to manage the vm space precisely!");
     exit(-1);
   }
-  better_free((void *)m);
+  better_free((void *)m); // free 0~136
 
   strcpy(p,str);
   printu("%s\n",p);
-  char *n = (char *)better_malloc(50);
+  char *n = (char *)better_malloc(50); // 224~312, following the Worst Fit Strategy
   
-  if(m != n)
+  // @assert n - m == 224
+
+  if(n - m != 224)
   {
     printu("your malloc is not complete.\n");
     exit(-1);
